2a7d3ebabb4d82059eb5d80dcc8d041e46eac3ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 20:44:16 +0000

    Deploy goodwin64/octo-doctor to github.com/goodwin64/octo-doctor.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30510d2bf2f42457fc4ce84afac0b7a9.js"
+  "/octo-doctor/precache-manifest.d07aaf3612cd0abfd7f147a7d2ad1d3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/octo-doctor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
