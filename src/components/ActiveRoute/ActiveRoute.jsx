ccb1d1b90a3fb32f73ad35bb3a3f02b36fa046ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 20:38:37 +0000

    Deploy goodwin64/octo-doctor to github.com/goodwin64/octo-doctor.git:gh-pages

--- src/components/ActiveRoute/ActiveRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Patients } from '../../pages/Patients/Patients';
-
-export function ActiveRoute() {
-  // A place to add routing in future
-  return (
-    <Patients/>
-  );
-}
--- src/components/PageWrapper/PageWrapper.styled.js
+++
@@ -1,7 +0,0 @@
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles((theme) => createStyles({
-  root: {
-    backgroundColor: theme.palette.background.default,
-  },
-}));
